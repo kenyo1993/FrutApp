bd641b517c7469c21a1a71981df80d345fdd2941
ken <i1022391@continental.edu.pe>
2018-06-04 18:08:10 -0500

    Pasando a una nueva actividad frutapp

---
+++ src/frutapp/frutapp/Activities/CatalogActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using frutapp.Model;
+
+namespace frutapp.Activities
+{
+    [Activity(Label = "CatalogActivity")]
+    public class CatalogActivity : Activity
+    {
+        ListView listViewCatalog;
+        List<Product> productList;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_catalog);
+
+
+            // Create your application here
+        }
+
+       
+    }
+}
---
+++ src/frutapp/frutapp/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using System;
+using frutapp.Activities;
+using Android.Content;
+
+namespace frutapp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button btngo;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            btngo = FindViewById<Button>(Resource.Id.button1);
+            btngo.Click += changeOfIntent;
+            // Set our view from the "main" layout resource
+            
+        }
+
+        private void changeOfIntent(object sender, EventArgs e)
+        {
+            Intent newActivity = new Intent(this, typeof(CatalogActivity));
+            this.StartActivity(newActivity);
+        }
+
+        
+
+    
+    }
+}
+
